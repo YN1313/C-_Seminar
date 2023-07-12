4447983f3ef380ff46a367028ecb786beb087206
DALER DADADZHANOVe <d.dadadzhanov87@gmail.com>
2023-07-12 16:42:12 +0600

    TASK_1

---
+++ TASK_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное
+// на само себя).
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+// Console.WriteLine(number * number);
+
+
+Console.WriteLine("Введите число: "); // подсказка для пользователя 
+int number = int.Parse(Console.ReadLine()); // Console.ReadLine() - считывает с консоли 
+int result = number * number; // храним результат вычисления числа само на себя
+Console.WriteLine("Квадрат числа = " + result); // конкатенация склеивание строки через
